e5bb2df5dea0c09e2ef9682f2b7f5ec979ea1a24
t-kimber <talia.kimber@gmail.com>
2021-04-29 14:11:36 +0200

    add kinase informed featurization scripts.

---
+++ features/example-ligand-chembl28-onehotsmiles-sequence-1k-subsample.py
@@ -0,0 +1,36 @@
+# You can only use UPPERCASE variables for the names
+# Values can ONLY be strings, numbers, lists (not tuples!) or dicts
+
+DATASET_CLS = "kinoml.datasets.chembl.ChEMBLDatasetProvider"
+DATASET_KWARGS = {
+    "path_or_url": "https://github.com/openkinome/kinodata/releases/download/v0.2/activities-chembl28-sample100_v0.2.zip",
+}
+
+PIPELINES = {
+    "ligand": [
+        ["kinoml.features.ligand.SmilesToLigandFeaturizer", {"style": "rdkit"}],
+        ["kinoml.features.ligand.OneHotSMILESFeaturizer", {}],
+        ["kinoml.features.core.PadFeaturizer", {"shape": "auto"}],
+    ],
+    "kinase": [
+        ["kinoml.features.protein.OneHotEncodedSequenceFeaturizer", {}],
+    ]
+
+}
+PIPELINE_AGG = "kinoml.features.core.Concatenated"
+PIPELINE_AGG_KWARGS = {}
+
+FEATURIZE_KWARGS = {"processes": 1}
+
+GROUPS = [
+    [
+        "kinoml.datasets.groups.CallableGrouper",
+        {"function": "lambda measurement: measurement.system.protein.name"},
+    ],  # by kinase
+    [
+        "kinoml.datasets.groups.CallableGrouper",
+        {"function": "lambda measurement: type(measurement).__name__"},
+    ],  # by measurement type
+]
+
+TRAIN_TEST_VAL_KWARGS = {"idx_train": 0.8, "idx_test": 0.1, "idx_val": 0.1}
