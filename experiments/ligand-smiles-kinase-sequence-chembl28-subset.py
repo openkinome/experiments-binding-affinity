8703eb9b019c4653b67b22d55c95e9bfbeef939c
t-kimber <talia.kimber@gmail.com>
2021-05-11 17:45:48 +0200

    add ML model for kinase informed setting.

--- experiments/ligand-smiles-kinase-sequence-chembl28-subset.py
+++ experiments/ligand-smiles-kinase-sequence-chembl28-subset.py
@@ -9,7 +9,7 @@ NPZ_FILES = [
 ]
 
 # Model -- specified with the full import path to the class object
-MODEL_CLS = "kinoml.ml.torch_models.ConvolutionNeuralNetworkRegression"
+MODEL_CLS = "kinoml.ml.torch_models.ConvolutionNeuralNetworkRegressionKinaseInformed"
 MODEL_KWARGS = {"hidden_shape": 350}  # input_shape is defined dynamically during training
 WITH_OBSERVATION_MODEL = True
 
