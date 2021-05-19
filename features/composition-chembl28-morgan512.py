647f5bc5b3b490ded735926df7f146e7acdde204
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2021-05-19 13:37:19 +0200

    update featurization examples

--- features/composition-chembl28-morgan512.py
+++ features/composition-chembl28-morgan512.py
@@ -1,20 +1,19 @@
-# ----
-# This input file is designed for features/featurize-template.ipynb
-# ----
-
 # You can only use UPPERCASE variables for the names
 # Values can ONLY be strings, numbers, lists (not tuples!) or dicts
 
 DATASET_CLS = "kinoml.datasets.chembl.ChEMBLDatasetProvider"
 DATASET_KWARGS = {
-    "path_or_url": "https://github.com/openkinome/kinodata/releases/download/v0.2/activities-chembl28-sample100_v0.2.zip",
+    "path_or_url": "https://github.com/openkinome/kinodata/releases/download/v0.2/activities-chembl28_v0.2.zip"
 }
 
 PIPELINES = {
     "ligand": [
         ["kinoml.features.ligand.SmilesToLigandFeaturizer", {}],
-        ["kinoml.features.ligand.GraphLigandFeaturizer", {}],
-    ]
+        ["kinoml.features.ligand.MorganFingerprintFeaturizer", {"nbits": 512, "radius": 2}],
+    ],
+    "kinase": [
+        ["kinoml.features.protein.AminoAcidCompositionFeaturizer", {}],
+    ],
 }
 PIPELINES_AGG = "kinoml.features.core.TupleOfArrays"
 PIPELINES_AGG_KWARGS = {}
@@ -29,5 +28,4 @@ GROUPS = [
         {"function": "lambda measurement: type(measurement).__name__"},
     ],  # by measurement type
 ]
-
 TRAIN_TEST_VAL_KWARGS = {"idx_train": 0.8, "idx_test": 0.1, "idx_val": 0.1}
