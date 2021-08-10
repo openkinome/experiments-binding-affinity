0acddb27fc64cfea5565dbc27b96d224471464e9
t-kimber <talia.kimber@gmail.com>
2021-08-10 11:44:53 +0200

    example kinase composition featurization.

---
+++ tests/features/test_featurization_kinase_informed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Featurization"
+
+echo "Kinase informed"
+
+echo "Running featurization for 'kinase-informed-morgan1024-composition-subsample'"
+python run_notebook.py features/featurize-template.ipynb features/kinase-informed-morgan1024-composition-subsample.py --overwrite
