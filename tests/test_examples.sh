07a4a26ca7597341a7852aae98318a5c40f9fdd4
David Schaller <schaller.david@yahoo.de>
2021-08-12 10:28:48 +0200

    add composition tests

--- tests/test_examples.sh
+++ tests/test_examples.sh
@@ -11,6 +11,12 @@ python run_notebook.py features/featurize-template.ipynb features/example-ligand
 echo "Running featurization for 'example-ligand-only-chembl28-ohesmiles-1k-subsample'"
 python run_notebook.py features/featurize-template.ipynb features/example-ligand-only-chembl28-ohesmiles-1k-subsample.py --overwrite
 
+echo "Running featurization for 'composition-chembl28-morgan1024'"
+python run_notebook.py features/featurize-template.ipynb features/composition-chembl28-morgan1024.py --overwrite
+
+echo "Running featurization for 'composition-chembl28-morgan512'"
+python run_notebook.py features/featurize-template.ipynb features/composition-chembl28-morgan512.py --overwrite
+
 
 
 echo "Running training for '000_example-ligand-only-chembl28-subset'"
