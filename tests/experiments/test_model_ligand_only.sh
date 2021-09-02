f0a4c4211d2dded12c8c2cacb8712eb2819e2310
t-kimber <talia.kimber@gmail.com>
2021-09-02 14:25:02 +0200

    test graph experiment

--- tests/experiments/test_model_ligand_only.sh
+++ tests/experiments/test_model_ligand_only.sh
@@ -7,10 +7,10 @@ echo "Models"
 echo "Ligand only"
 
 echo "Running model for '001_example-ligand-only-morgan1024-subset'"
-python run_notebook.py experiments/torch-train-test-debug-template.ipynb experiments/001_example-ligand-only-morgan1024-subset.py --overwrite
+# python run_notebook.py experiments/torch-train-test-debug-template.ipynb experiments/001_example-ligand-only-morgan1024-subset.py --overwrite
 
 echo "Running model for '001_example-ligand-only-smiles-subset.py'"
 # python run_notebook.py experiments/torch-train-test-debug-template.ipynb experiments/001_example-ligand-only-smiles-subset.py --overwrite
 
-# echo "Running model for '001_example-ligand-only-graph-subset'"
-# python run_notebook.py experiments/torch-train-test-debug-template.ipynb experiments/001_example-ligand-only-graph-subset.py --overwrite
+echo "Running model for '001_example-ligand-only-graph-subset'"
+python run_notebook.py experiments/torch-train-test-debug-template.ipynb experiments/001_example-ligand-only-graph-subset.py --overwrite
